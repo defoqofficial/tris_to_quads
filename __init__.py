d3f98924c1095abe3dbfe00ccf1f4cf69cf43d11
defoqofficial <118861330+defoqofficial@users.noreply.github.com>
2026-06-22 17:59:10 +0200

    Add logging configuration for better diagnostics
    - Configure logging to help with debugging conversion failures
    - Set up logging handlers in module initialization

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Q-Morph Quad Meshing",
     "author": "Q-Morph Addon",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (5, 1, 0),
     "location": "View3D > Sidebar > Q-Morph Quads",
     "description": "Convert triangle meshes to optimized quad meshes using Q-Morph",
@@ -9,6 +9,18 @@ bl_info = {
 }
 
 import bpy
+import logging
+import sys
+
+# Configure logging for debugging
+logger = logging.getLogger("q_morph_quads")
+if not logger.handlers:
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("[Q-Morph] %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
 
 from .operators.convert import MESH_OT_qmorph_convert_to_quads, menu_func
 from .ui.panel import QMorphProperties, VIEW3D_PT_qmorph_quads
@@ -25,6 +37,7 @@ def register():
         bpy.utils.register_class(cls)
     bpy.types.Scene.qmorph_props = bpy.props.PointerProperty(type=QMorphProperties)
     bpy.types.VIEW3D_MT_edit_mesh_clean.append(menu_func)
+    logger.info(f"Q-Morph Quad Meshing v{bl_info['version']} registered")
 
 
 def unregister():
@@ -32,6 +45,7 @@ def unregister():
     del bpy.types.Scene.qmorph_props
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
+    logger.info("Q-Morph Quad Meshing unregistered")
 
 
 if __name__ == "__main__":
